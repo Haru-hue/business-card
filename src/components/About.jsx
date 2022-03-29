3c9084613af259c5de2eae5d8f8ff3026c2237ba
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-03-29 11:46:06 +0100

    First upload

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function About () {
+    return (
+        <div className="about-section">
+            <h4>About</h4>
+            <p>I am a front-end developer that loves creating websites and
+                designs that I find simple yet beautiful. I love learning 
+                about new things, and always trying to better myself.
+                I build websites from scratch with Figma then code with 
+                HTML/CSS.
+            </p>
+        </div>
+    )
+}
---
+++ src/components/Interests.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Interests () {
+    return (
+        <div className="interest-section">
+            <h4>Interests</h4>
+            <p>
+                Travelling. Foodie. Anime conossieur. Music lover.
+                UI/UX Design. Animation. Animal lover.
+                2D Art. Movie and TV critic
+            </p>
+        </div>
+    )
+}
+
+export default Interests
